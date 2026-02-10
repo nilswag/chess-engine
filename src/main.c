cdbbb41336fbd5e31ed291c76347028e9469c94b
nilswag <nilswagemaker05@icloud.com>
2026-02-10 16:56:44 +0100

    Started working on board representation

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
-#include "base/log.h"
 
 int main(void)
 {
-    LOG_INFO("%s\n", "Hello INFO");
-    LOG_WARN("%s\n", "Hello WARNING");
-    LOG_ERROR("%s\n", "Hello ERROR");
-
+    
     return 0;
 }
