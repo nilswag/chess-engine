6cca4abe937317e108864892cd99e56a4cac0d7e
nilswag <nilswagemaker05@icloud.com>
2026-03-04 22:39:10 +0100

    Fixed small fen parsing bugs and started working on printing board

--- lce-cli/src/main.c
+++ lce-cli/src/main.c
@@ -1,9 +1,20 @@
-#include "lce/util/logger.h"
+#include <stdio.h>
+
 #include "lce/core/board.h"
 
 void print_board(LCEBoard* board)
 {
-
+    for (int i = 63; i > 0; i--)
+    {
+        for (int type = 0; type < LCE_PIECE_TYPE_NR; type++)
+        {
+            for (int color = 0; type < LCE_PIECE_COLOR_NR; color++)
+            {
+                
+            }
+        }
+        if (!(i % 8)) putchar('\n');
+    }
 }
 
 int main(void)
