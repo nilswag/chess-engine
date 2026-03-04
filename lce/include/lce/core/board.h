1bb27faf5ca6eac9bff59053b99802c7d37bcfcd
nilswag <nilswagemaker05@icloud.com>
2026-03-04 20:11:36 +0100

    Added board enums

---
+++ lce/include/lce/core/board.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdint.h>
+
+typedef enum LCEPieceType
+{
+    LCE_PIECE_TYPE_PAWN = 0,
+    LCE_PIECE_TYPE_KNIGHT,
+    LCE_PIECE_TYPE_BISHOP,
+    LCE_PIECE_TYPE_ROOK,
+    LCE_PIECE_TYPE_QUEEN,
+    LCE_PIECE_TYPE_KING,
+    LCE_PIECE_TYPE_NR
+} LCEPieceType;
+
+typedef enum LCEPieceColor 
+{
+    LCE_PIECE_COLOR_WHITE = 0,
+    LCE_PIECE_COLOR_BLACK,
+    LCE_PIECE_COLOR_NR
+} LCEPieceColor;
+
+typedef struct LCEBoard
+{
+    uint64_t board[LCE_PIECE_COLOR_NR][LCE_PIECE_TYPE_NR];
+} LCEBoard;
+
+void lce_board_init(LCEBoard* board);
+void lce_board_destroy(LCEBoard* board);
+
+void lce_board_load_fen(LCEBoard* board, const char* fen_str);
---
+++ lce/src/core/board.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+#include "lce/core/board.h"
+
+void lce_board_init(LCEBoard* board)
+{
+
+}
+
+void lce_board_destroy(LCEBoard* board)
+{
+    
+}
+
+void lce_board_load_fen(LCEBoard* board, const char* fen_str)
+{
+    for (int i = 0; i < strlen(fen_str); i++)
+    {
+
+    }
+}
