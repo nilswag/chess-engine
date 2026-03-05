5cb6eb625fd0e149276277726f038172b117019b
nilswag <nilswagemaker05@icloud.com>
2026-03-05 20:42:22 +0100

    aaa

--- lce-cli/src/main.c
+++ lce-cli/src/main.c
@@ -7,7 +7,7 @@ int main(void)
     LCEBoard board;
     lce_board_init(&board);
 
-    lce_load_fen(&board, "8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1");
+    lce_load_fen(&board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
     lce_print_board(&board);
 
     lce_board_deinit(&board);
--- lce-cli/src/util/util.c
+++ lce-cli/src/util/util.c
@@ -15,7 +15,7 @@ LCEError lce_print_board(LCEBoard* board)
 
     for (int rank = 7; rank >= 0; rank--)
     {
-        printf("%d", rank);
+        printf("%d ", rank);
 
         for (int file = 0; file < 8; file++)
         {
@@ -41,7 +41,7 @@ LCEError lce_print_board(LCEBoard* board)
         putchar('\n');
     }
 
-    printf(" ");
+    printf("\n  ");
     for (int i = 0; i < 8; i++)
         printf("%c", rank_chars[i]);
     putchar('\n');
