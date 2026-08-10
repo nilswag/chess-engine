797943012207329627e907adc47cca81c49591ed
nilswag <nilswagemaker05@icloud.com>
2026-08-10 17:01:15 +0200

    Added black pawn attack/target squares

--- src/generation/pawn.cpp
+++ src/generation/pawn.cpp
@@ -27,4 +27,27 @@ namespace lce::generation::pawn
 		uint64_t double_push = w_pawn_double_push_targets(pawns, empty);
 		return single_push | double_push;
 	}
+
+	uint64_t b_pawn_attacks(uint64_t pawns)
+	{
+		return bb::move_south_east(pawns) | bb::move_south_west(pawns);
+	}
+
+	uint64_t b_pawn_single_push_targets(uint64_t pawns, uint64_t empty)
+	{
+		return bb::move_south(pawns) & empty;
+	}
+
+	uint64_t b_pawn_double_push_targets(uint64_t pawns, uint64_t empty)
+	{
+		uint64_t step = bb::move_south(pawns & bb::constants::RANK_2);
+		return bb::move_south(step & empty) & empty;
+	}
+
+	uint64_t b_pawn_targets(uint64_t pawns, uint64_t empty)
+	{
+		uint64_t single_push = b_pawn_single_push_targets(pawns, empty);
+		uint64_t double_push = b_pawn_double_push_targets(pawns, empty);
+		return single_push | double_push;
+	}
 }
--- src/generation/pawn.h
+++ src/generation/pawn.h
@@ -24,4 +24,24 @@ namespace lce::generation::pawn
 	* @brief Returns set of single and double push targets for white pawns
 	*/
 	uint64_t w_pawn_targets(uint64_t pawns, uint64_t empty);
+
+	/**
+	* @brief Returns set of all pseudo-legal pawn attacks for black
+	*/
+	uint64_t b_pawn_attacks(uint64_t pawns);
+
+	/**
+	* @brief Returns set of single push targets for black pawns
+	*/
+	uint64_t b_pawn_single_push_targets(uint64_t pawns, uint64_t empty);
+
+	/**
+	* @brief Returns set of double push targets for black pawns
+	*/
+	uint64_t b_pawn_double_push_targets(uint64_t pawns, uint64_t empty);
+
+	/**
+	* @brief Returns set of single and double push targets for black pawns
+	*/
+	uint64_t b_pawn_targets(uint64_t pawns, uint64_t empty);
 }
